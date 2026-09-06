c5a09641e105decc70fca2cbd0405dfca2035ad3
Igor <bulgar.1991igor@gmail.com>
2026-09-06 13:32:13 +0300

    task1: header and footer

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,17 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Request } from './pages/request/request';
 
-export const routes: Routes = [
+const mainLayoutRoutes: Routes = [
   { path: '', component: Home },
   { path: 'request/:serviceId', component: Request },
   { path: '**', redirectTo: '' },
+];
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@components/main-layout/main-layout.component').then((m) => m.MainLayoutComponent),
+    children: mainLayoutRoutes,
+  },
 ];
